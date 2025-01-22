2b1de9727b3b500268bc6b5847fbdd965615f3ed
Ruchi Patel <rucipatel9@gmail.com>
2025-01-22 00:18:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ca-central-1"
-  access_key = "AKIA2ZIONCXYCRMRI2WJ"
-  secret_key = "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"
+  #access_key = ""
+  #secret_key = ""
   
 }
 
